82bf0fcf835d6a19fb3e8da08a61fe6292dcdccf
Ahmad-Abulaban <eng.ahmad090@gmail.com>
2021-10-03 22:09:31 +0300

    Add Header & Footer

---
+++ src/component/Pages/FavoirteList.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+class FavoirteList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Favoirte List</h1>
+        <Card style={{ width: "18rem", marginBottom: '25px' }}>
+          <Card.Body>
+            <Card.Title>Favoirte List</Card.Title>
+            <Card.Text>Favoirte List</Card.Text>
+          </Card.Body>
+          <Card.Text>Favoirte List</Card.Text>
+          <Card.Text>Favoirte List</Card.Text>
+          <Card.Text>Favoirte List</Card.Text>
+        </Card>
+        <Card style={{ width: "18rem", marginBottom: '25px' }}>
+          <Card.Body>
+            <Card.Title>Favoirte List</Card.Title>
+            <Card.Text>Favoirte List</Card.Text>
+          </Card.Body>
+          <Card.Text>Favoirte List</Card.Text>
+          <Card.Text>Favoirte List</Card.Text>
+          <Card.Text>Favoirte List</Card.Text>
+        </Card>
+        <Card style={{ width: "18rem", marginBottom: '25px' }}>
+          <Card.Body>
+            <Card.Title>Favoirte List</Card.Title>
+            <Card.Text>Favoirte List</Card.Text>
+          </Card.Body>
+          <Card.Text>Favoirte List</Card.Text>
+          <Card.Text>Favoirte List</Card.Text>
+          <Card.Text>Favoirte List</Card.Text>
+        </Card>
+        <Card style={{ width: "18rem", marginBottom: '25px' }}>
+          <Card.Body>
+            <Card.Title>Favoirte List</Card.Title>
+            <Card.Text>Favoirte List</Card.Text>
+          </Card.Body>
+          <Card.Text>Favoirte List</Card.Text>
+          <Card.Text>Favoirte List</Card.Text>
+          <Card.Text>Favoirte List</Card.Text>
+        </Card>
+        <Card style={{ width: "18rem", marginBottom: '25px' }}>
+          <Card.Body>
+            <Card.Title>Favoirte List</Card.Title>
+            <Card.Text>Favoirte List</Card.Text>
+          </Card.Body>
+          <Card.Text>Favoirte List</Card.Text>
+          <Card.Text>Favoirte List</Card.Text>
+          <Card.Text>Favoirte List</Card.Text>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default FavoirteList;
---
+++ src/component/Pages/Home.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import './Home.css';
+// import LoginButton from './component/LoginButton';
+
+class Login extends React.Component {
+  render() {
+    return(
+      <>
+      <h1>Home Page</h1>
+      <Card style={{ width: '18rem', marginBottom: '25px', marginTop: '50px' }}>
+        <Card.Body>
+          <Card.Title>HomePage</Card.Title>
+          <Card.Text>
+            Click Below to HomePage
+          </Card.Text>
+          {/* <LoginButton /> */}
+        </Card.Body>
+        <Card.Text>
+          Click Below to HomePage
+        </Card.Text>
+      </Card>
+        <Card style={{ width: '18rem', marginBottom: '25px' }}>
+          <Card.Body>
+            <Card.Title>HomePage</Card.Title>
+            <Card.Text>
+              Click Below to HomePage
+            </Card.Text>
+            {/* <LoginButton /> */}
+          </Card.Body>
+          <Card.Text>
+            Click Below to HomePage
+          </Card.Text>
+        </Card>
+        <Card style={{ width: '18rem', marginBottom: '25px' }}>
+          <Card.Body>
+            <Card.Title>HomePage</Card.Title>
+            <Card.Text>
+              Click Below to HomePage
+            </Card.Text>
+            {/* <LoginButton /> */}
+          </Card.Body>
+          <Card.Text>
+            Click Below to HomePage
+          </Card.Text>
+        </Card>
+        <Card style={{ width: '18rem', marginBottom: '25px' }}>
+          <Card.Body>
+            <Card.Title>HomePage</Card.Title>
+            <Card.Text>
+              Click Below to HomePage
+            </Card.Text>
+            {/* <LoginButton /> */}
+          </Card.Body>
+          <Card.Text>
+            Click Below to HomePage
+          </Card.Text>
+        </Card>
+        </>
+    )
+  }
+}
+
+export default Login;
