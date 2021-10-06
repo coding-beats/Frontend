356f04bd01c2e7984bfbfcab392bc622afb00b90
husseinalkarmi <husseinalkarmi012@gmail.com>
2021-10-06 21:26:11 +0300

    aboutUs Edit

--- src/component/Pages/AboutUs.js
+++ src/component/Pages/AboutUs.js
@@ -22,7 +22,9 @@ class AboutUs extends React.Component {
         </section>
         <div id="container">
           {/* <h1 style={{ color: "black", fontFamily: "cursive" }}> Our Team </h1> */}
-          < h1 style ={{color:"black",position:"relative", bottom:"280px",fontFamily:"cursive", textAlign:"center", textShadow:"2px 2px black" }}>Coding & Beats Organization <br></br> <br></br> Meet Our Team </h1>
+          < h1 style ={{color:"black",position:"relative", bottom:"280px",fontFamily:"cursive", textAlign:"center", textShadow:"2px 2px black" }}>Special Thanks From Our Coding & Beats Team </h1>
+
+          < h1 style ={{color:"black",position:"relative", marginTop:"-30px",fontFamily:"cursive", textAlign:"center", textShadow:"2px 2px black" }}> Meet Coding & Beats Team </h1>
           
           <hr />
           <div className="card">
